7df0fa2baabf959a623c8144363ef994f24be574
AttAditya <attachment.aditya@gmail.com>
2026-06-16 02:17:46 +0530

    feat: link to project page

--- portable/src/components/block/latest.tsx
+++ portable/src/components/block/latest.tsx
@@ -1,5 +1,8 @@
+import { Bbq } from "@attaditya/iconoir-preact";
 import { Linklet } from "@components/kit/linklet";
 import { Section } from "@components/kit/section";
+import { Button } from "@components/ui/interactive/button";
+import { Link } from "@components/ui/interactive/link";
 import { Container } from "@components/ui/structure/container";
 import { Heading } from "@components/ui/text/heading";
 import { useClasses } from "@styles";
@@ -30,6 +33,13 @@ export function Latest() {
           title="Pilot"
         />
       </Container>
+
+      <Linklet
+        icon="BbqRegular"
+        url="/projects"
+        newTab={false}
+        title="See all projects"
+      />
     </Section>
   )
 }
