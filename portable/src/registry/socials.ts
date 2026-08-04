d62da89f7a3625098a9c4b6a6b1bdea98a8a4c1e
AttAditya <attachment.aditya@gmail.com>
2026-08-04 17:31:26 +0530

    fix: change gitlab url

--- portable/src/registry/socials.ts
+++ portable/src/registry/socials.ts
@@ -10,7 +10,7 @@ export const SOCIALS: {
     },
     {
       icon: "GithubRegular",
-      url: "https://github.com/Its-AttAditya",
+      url: "https://gitlab.com/AttAditya",
     },
     {
       icon: "LinkedinRegular",
@@ -25,7 +25,7 @@ export const SOCIALS: {
     {
       icon: "JournalRegular",
       title: "Resume",
-      url: "https://github.com/Its-AttAditya/resume",
+      url: "https://gitlab.com/AttAditya/resume",
     }
   ]
 ];
