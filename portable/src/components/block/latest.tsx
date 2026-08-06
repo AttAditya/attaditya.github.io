c674ef90da1d737132cf236fa855d85d627c7810
AttAditya <attachment.aditya@gmail.com>
2026-08-06 20:43:36 +0530

    chore: remove unused libs

--- portable/src/components/block/latest.tsx
+++ portable/src/components/block/latest.tsx
@@ -1,8 +1,5 @@
-import { Bbq } from "@attaditya/iconoir-preact";
 import { Linklet } from "@components/kit/linklet";
 import { Section } from "@components/kit/section";
-import { Button } from "@components/ui/interactive/button";
-import { Link } from "@components/ui/interactive/link";
 import { Container } from "@components/ui/structure/container";
 import { Heading } from "@components/ui/text/heading";
 import { useClasses } from "@styles";
